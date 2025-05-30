331d3469ad190667e260b634453dd4a772f7600e
kamineni sravani <kaminenisravani12@gmail.com>
2025-05-30 19:04:04 +0530

    Update roberta_model.py

--- backend/models/roberta_model.py
+++ backend/models/roberta_model.py
@@ -1,1 +1,17 @@
+from transformers import pipeline
+
+# Load your fine-tuned model from the local directory
+classifier = pipeline(
+    "text-classification",
+    model="./models/model_output/checkpoint-634",
+    tokenizer="./models/model_output/checkpoint-634"
+)
+
+
+def get_verdict(text):
+    result = classifier(text)[0]
+    label = result['label']
+    score = result['score']
+    verdict = "REAL" if label == "LABEL_1" else "FAKE"
+    return {"verdict": verdict, "confidence": score}
 
